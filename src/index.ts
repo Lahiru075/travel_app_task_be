2364c11bdc7b855f8e77435ddaa86f822ca75fa3
Lahiru075 <sanjeewalahiru057@gmail.com>
2026-04-28 14:16:18 +0530

    setup server entry point and database connection

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+dotenv.config();
+
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI as string;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    })
+);
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log('DB Connected');
+    })
+    .catch((error) => {
+        console.error(`DB connection fail: ${error}`);
+    });
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on port ${SERVER_PORT}`);
+});
