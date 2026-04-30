9e2bf4bbac012e5d2351eb097fc5c785cd2994b8
Lahiru075 <sanjeewalahiru057@gmail.com>
2026-04-30 16:41:37 +0530

    implement refresh token endpoint

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import { User } from "../model/User";
 import { signAccessToken , signRefreshToken } from "../utils/token";
+import jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response) => {
     try {
@@ -55,3 +56,22 @@ export const login = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Error logging in user", error });
     }
 }
+
+export const refreshToken = async (req: Request, res: Response) => {
+    const { token } = req.body;
+
+    if (!token) return res.status(401).json({ message: "Refresh Token is required" });
+
+    try {
+        const payload: any = jwt.verify(token, process.env.JWT_REFRESH_SECRET as string);
+        
+        const user = await User.findById(payload.sub);
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        const newAccessToken = signAccessToken(user);
+
+        res.status(200).json({ accessToken: newAccessToken });
+    } catch (error) {
+        res.status(403).json({ message: "Invalid or expired refresh token" });
+    }
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login, register } from "../controller/user.controller";
+import { login, refreshToken, register } from "../controller/user.controller";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/refresh-token", refreshToken);
 
 export const authRoutes = router;
